706a07a2fc9c31aaadc83e8e90ffbdb4e2c42975
newExpand <120312998+newExpand@users.noreply.github.com>
2025-02-27 15:02:36 +0900

    test: Add test files for Phase 4 integration testing

---
+++ test.tsx
@@ -0,0 +1,10 @@
+
+            import React from 'react';
+            
+            const TestComponent: React.FC = () => {
+              const isEnabled = true;
+              return <div>{isEnabled && 'Enabled'}</div>;
+            };
+            
+            export default TestComponent;
+          
